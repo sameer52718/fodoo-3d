80dac42d7553b464bbc9d5e2fc4b552800eb1005
sameer52718 <alisameer52718@gmail.com>
2025-07-01 20:34:31 +0500

    fix

--- app/(website)/page.js
+++ app/(website)/page.js
@@ -1,3 +1,21 @@
-export default function page() {
-  return <div>Home</div>;
+'use client';
+
+import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
+export default function Page() {
+  const router = useRouter();
+  const { token } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (!token) {
+      router.replace('/dashboard/login'); // redirect to login if not authenticated
+    } else {
+      router.replace('/dashboard'); // redirect to login if not authenticated
+    }
+  }, [token, router]);
+
+
+  return null;
 }
